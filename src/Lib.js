9d0b622a7fff90473354373756e5a2f2dab05d3d
Yangkai.Shen <237497819@qq.com>
2019-11-15 20:21:01 +0800

    :recycle: 隐藏移动端提示

--- src/Lib.js
+++ src/Lib.js
@@ -54,13 +54,13 @@ class Lib extends Component {
           <Result
             icon={<SvgIcon name="smile" style={style.svgIcon} />}
             title="请使用 PC 端打开排版工具"
-            subTitle="更多 Markdown Nice 信息，请扫码关注公众号「牧码啦」"
-            extra={
-              <img
-                alt=""
-                src="https://draw-wechat.oss-cn-hangzhou.aliyuncs.com/%E4%BA%8C%E7%BB%B4%E7%A0%81_20190823124950.gif"
-              />
-            }
+            // subTitle="更多 Markdown Nice 信息，请扫码关注公众号「牧码啦」"
+            // extra={
+            //   <img
+            //     alt=""
+            //     src="https://draw-wechat.oss-cn-hangzhou.aliyuncs.com/%E4%BA%8C%E7%BB%B4%E7%A0%81_20190823124950.gif"
+            //   />
+            // }
           />
         )}
       </Provider>
